8bdf7106cbe05bb384cb1c6195f5dea68526b706
CrenierAmaury <amaurycrenier@gmail.com>
2022-06-22 16:28:49 +0200

    add sql script many to many

--- init_db/createUsers.sql
+++ init_db/createUsers.sql
@@ -8,3 +8,20 @@ CREATE TABLE users(
 
 CREATE INDEX username_index ON users(username);
 CREATE INDEX useremail_index ON users(useremail);
+
+
+CREATE TABLE roles(
+    roleid SERIAL PRIMARY KEY,
+    rolename VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE usersroles(
+    userid integer NOT NULL,
+    roleid integer NOT NULL,
+    CONSTRAINT "contacts-roles_pkey" PRIMARY KEY (userid)
+        INCLUDE(roleid),
+    CONSTRAINT users_fkey FOREIGN KEY (userid)
+        REFERENCES public.users (userid) MATCH SIMPLE,
+    CONSTRAINT roles_fkey FOREIGN KEY (roleid)
+        REFERENCES public.roles (roleid) MATCH SIMPLE
+);
