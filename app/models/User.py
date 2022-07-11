a87b99e5783250d5197fbe66ad85fa494745bf85
philippe.stons <philippe.stons@bstorm.be>
2022-07-11 16:00:03 +0200

    [SQL ALCHEMY]

--- app/models/User.py
+++ app/models/User.py
@@ -1,17 +1,21 @@
-from sys import stderr
+from app import db
 
-
-class User:
-    def __init__(self, userid, username, userpassword, useremail, userdescription) -> None:
-        self.userid = userid
-        self.username = username
-        self.userpassword = userpassword
-        self.useremail = useremail
-        self.userdescription = userdescription
-        self.roles = []
-
-    def isAdmin(self):
-       print(self.roles, file=stderr)
-       print("ADMIN" in self.roles, file=stderr)
-       isAdmin = "ADMIN" in self.roles
-       return isAdmin
+class User(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    useremail = db.Column(db.String(100), unique=True, nullable=False)
+    userpassword = db.Column(db.String(255), nullable=False)
+    userdescription = db.Column(db.String(255), nullable=True)
+    # def __init__(self, userid, username, userpassword, useremail, userdescription) -> None:
+    #     self.userid = userid
+    #     self.username = username
+    #     self.userpassword = userpassword
+    #     self.useremail = useremail
+    #     self.userdescription = userdescription
+    #     self.roles = []
+    #
+    # def isAdmin(self):
+    #    print(self.roles, file=stderr)
+    #    print("ADMIN" in self.roles, file=stderr)
+    #    isAdmin = "ADMIN" in self.roles
+    #    return isAdmin
